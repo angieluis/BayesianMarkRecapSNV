e4f5809aef51afc1b960f0286e9d79ad0613ce7f
angieluis <angela.d.luis@gmail.com>
2018-03-27 08:56:12 -0600

    new initial values and known state functions

--- SimulatedData/MultiStateInfection.R
+++ SimulatedData/MultiStateInfection.R
@@ -167,7 +167,7 @@ cat("
     for (i in 1:nind){  
     # Define probabilities of State(t+1) given State(t)
     for (t in f[i]:(n.occasions-1)){
-    ### for proper process model, here say psiSI[t]=beta*I[t] and define beta in priors instead of psiSI (need a logit transform probably)
+    ### for proper process model, here say psiSI[t]<-beta*I[t] and define beta in priors instead of psiSI (need a logit or mlogit transform probably? Or is it fine given the priors?)
     ps[1,i,t,1] <- phiS[t] * (1-psiSI[t])
     ps[1,i,t,2] <- phiS[t] * psiSI[t] 
     ps[1,i,t,3] <- 1-phiS[t]
@@ -215,7 +215,7 @@ sink()
 ###changed this so can't go back to S from I. 
 #If observed as I, then not seen, and seen again later, when not seen must have been I. 
 #If observed as S, not seen, then observed as S again, then must be S.
-#Allows us to fill in a lot.
+#Allows us to fill in a lot. And should speed up computation time
 known.state.SIms=function(ms,notseen){ 
   # notseen: label for 'not seen' #here is 3 (Dead)
   state <- ms
@@ -240,90 +240,24 @@ known.state.SIms=function(ms,notseen){
 
 
 # Function to create initial values for unknown z  
-#filling in something for every 3 (not captured)
-### changing this so can't go back to I from S. If observed as I must remain I. & and if observed later as S must be S now.
-### I don't think I need to do any of this. These are just initial values so can be a random sample of 1 and 2 for all those not filled in above. The process model will make it so can't go back because that transition prob is 0. Right? Need to redo.
-#Do I only need to fill in the ones that are NA in known state matrix generated with code above for known states? They don't seem to work together. Do they? How?  
+#filling in something for every 3/NA (not captured) in known.state function after initial capture 
+#randomly samples from alive states for those that are unknown after initial capture
+# reversed from known.state - want NAs everywhere we know and 1:f. 
 SIms.init.z <- function(ch, f){ 
-  for (i in 1:dim(ch)[1]){
-    if(f[i]>1){ch[i,1:(f[i]-1)] <- NA}
-    }
-  states <- max(ch, na.rm = TRUE)
-  known.states <- 1:(states-1)
-  ch2 <- ch
-  
-  for(i in 1:dim(ch2)[1]){
-    if(length(which(ch2[i,]==2))>0){ #filling in S's where can
-      minI=min(which(ch2[i,]==2))
-      maxI=max(which(ch2[i,]==2))
-      ch2[i,minI:maxI]=2}
-    if(length(which(ch2[i,]==1))>0){  #filling in I's where can
-      minI=min(which(ch2[i,]==1))
-      maxI=max(which(ch2[i,]==1))
-      ch2[i,minI:maxI]=1}
-  }
-  
-  ch3 <- matrix(NA,dim(ch2)[1],dim(ch2)[2])
-  v <- which(ch2==states,arr.ind=TRUE)
-   
-  for(i in 1:dim(v)[1]){
-    #need to account for when never seen as 2 in the first case & never seen as 1 in the second case (will get error)
-    #states that indiv ever had
-    si=unique(ch[v[i,1],])[-which(unique(ch[v[i,1],])==3)]
-    si=si[which(is.finite(si))]
-    if(length(si)==2){ # identified as both uninfected and infected
-       ch3[v[i,1],v[i,2]] <- ifelse(
-          v[i,2]>min(which(ch[v[i,1],]==2)),2, #if this is after seen as infected, then still infected
-            ifelse(v[i,2]<max(which(ch[v[i,1],]==1)),1, #if before seen as uninfected, then uninfected
-            sample(c(1,2)))) #otherwise choose randomly between the 2
-       }
-    if(length(si)==1){
-        if(si==2){ # identified as only infected
-           ch3[v[i,1],v[i,2]] <- ifelse(
-             v[i,2]>min(which(ch[v[i,1],]==2)),2, #if this is after seen as infected, then still infected
-             sample(c(1,2)))} #otherwise choose randomly between the 2
-        if(si==1){ # identified as only uninfected
-           ch3[v[i,1],v[i,2]] <- ifelse(
-             v[i,2]<max(which(ch[v[i,1],]==1)),1, #if before seen as uninfected, then uninfected
-            sample(c(1,2)))} #otherwise choose randomly between the 2
-      }
-      
-    }
-  return(ch3)
-}
 
-
-# Function to create initial values for unknown z
-# randomly samples from alive states for every time an animal wasn't caught
-ms.init.z <- function(ch, f){
-  for (i in 1:dim(ch)[1]){ch[i,1:f[i]] <- NA}
+  zch = known.state.SIms(ch, 3)
   states <- max(ch, na.rm = TRUE) #in current example, 3 states, 3rd is dead
   known.states <- 1:(states-1) # remove unknown state
-  v <- which(ch==states) #which ones are not caught?
-  ch[-v] <- NA        # all the known states make NA
-  ch[v] <- sample(known.states, length(v), replace = TRUE) # unknown sample from known
-  return(ch)
-}
-
-# Function to create initial values for unknown z
-# randomly samples from alive states for only times not filled in by known.state function
-ms.init.z2 <- function(ch, f){
   
-  zch = known.state.SIms(ch, 3)
-    
-  for (i in 1:dim(zch)[1]){
-    zch[i,f]<-ch[i,f] 
-    ### need to change zch to correct form w 3s and not NAs
-    zch[i,1:f[i]] <- NA
-    }
-  states <- max(zch, na.rm = TRUE) #in current example, 3 states, 3rd is dead
-  known.states <- 1:(states-1) # remove unknown state
-  v <- which(zch==states) #which ones are not caught?
-  zch[-v] <- NA        # all the known states make NA
-  zch[v] <- sample(known.states, length(v), replace = TRUE) # unknown sample from known
+  for (i in 1:dim(ch)[1]){ 
+    v=which(is.na(zch[i,(f[i]+1):dim(zch)[2]])) # all unknown after first cap
+    zch[i,-(v+f[i])] <- NA        # all the known states make NA #doesn't work when all time points are known because length of v is 0
+    if(length(v)==0){zch[i,f[i]:dim(zch)[2]]<-NA}
+    zch[i,v+f[i]] <- sample(known.states, length(v), replace = TRUE) # unknown sample from known
+    }  
   return(zch)
 }
-### this isn't working yet.
+
 
 
 
@@ -333,7 +267,7 @@ ms.init.z2 <- function(ch, f){
 bugs.data <- list(y = rCH, f = f, n.occasions = dim(rCH)[2], nind = dim(rCH)[1], z = known.state.SIms(rCH, 3))
 
 # Initial values
-inits <- function(){list(mean.phi = runif(2, 0, 1), mean.psi = runif(1, 0, 1), mean.p = runif(2, 0, 1), z = ms.init.z(rCH, f))}  
+inits <- function(){list(mean.phi = runif(2, 0, 1), mean.psi = runif(1, 0, 1), mean.p = runif(2, 0, 1), z = SIms.init.z(rCH, f))}  
 
 # Parameters monitored
 parameters <- c("mean.phi", "mean.psi", "mean.p")
@@ -349,11 +283,8 @@ date()
 ms <- jags(bugs.data, inits, parameters, "msSI.bug", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb)
 date()
 
-##### when use SIms.init.z, getting error: node inconsistent with parents z[8,5] (changes with different iterations)... This probably has to do with init.z values not being consistent with what I've specified. Not sure what to do. In the CJS, they were all set to 0, but here they were set to observed states. Should I change them to 3? That would be a problem for those that saw again (but didn't know the state at the time)
+##### getting error: node inconsistent with parents z[8,5] (or z[2,5] - changes with different iterations)... This probably has to do with init.z and/or known.state.SIms values not being consistent with what I've specified. Prob known state or where transition parameters are 0 or 1.
 
-######## when use ms.init.z get error Error in setParameters(init.values[[i]], i) : Error in node z[1,2]  
-#Cannot overwrite value of observed node
-#sounds like I don't have to fill in initial values for those I filled in with known.state.SIms
 
 
 print(ms, digits = 3)
