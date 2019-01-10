f2f6a197f10fa07695f22d85df16073b97b6cedd
angieluis <angela.d.luis@gmail.com>
2019-01-10 08:41:00 -0700

    added month and sex covariates

--- RealData/robust_CJS_weekly_max.R
+++ RealData/robust_CJS_weekly_max.R
@@ -6,7 +6,7 @@
 #modeled on a weekly scale
 
 #################################specify model in BUGS language
-sink("robust_CJS_weekly_phi_ndvi_0_ndvi_1_tmax_3_tmin_5_p_dot_c_dot.bug")
+sink("robust_CJS_weekly_maxcov.bug")
 cat("
     model{
     
@@ -17,55 +17,55 @@ cat("
     
     alpha.ndvi_1 ~ dnorm(0, 0.4)T(-10,10) #prior for coefficient on NDVI lag 1
     alpha.male ~ dnorm(0, 0.4)T(-10,10)   #prior for coef on sex=male
+
     
-    # for month covariate, what are all the months in the dataset 
-    # (e.g, may not ever have trapped in Jan)
-    uniq.months <- sort(unique(covariate.month))
-      # don't estimate a beta coefficient for the first month (assume 0),
-      # then estimate a coefficent for all other months (length(uniq.months)-1)
-    for(m in 1:(length(uniq.months)-1)){
+      # don't estimate a beta coefficient for Jan (assume 0),
+      # then estimate a coefficent for all other months 
+    for(m in 1:11){
       alpha.month[m] ~ dnorm(0, 0.4)T(-10,10)   #prior for coefficients on months
       alpha.month.ndvi_1[m] ~ dnorm(0, 0.4)T(-10,10)   #prior for coef on the interaction between month and ndvi lag 1
     }
+    alpha.month.use <- c(0,alpha.month)
+    alpha.month.ndvi_1.use <- c(0,alpha.month.ndvi_1)
     
     ###### Priors for recapture prob
     #mean.p ~ dnorm(0, 0.4)T(-10,10)       # prior for p
     #mean.c ~ dnorm(0, 0.4)T(-10,10)       # prior for c
-    sigma.0 ~ dnorm(0, 0.4)T(-10,10)   #prior for intercept
-    
-    sigma.recap ~ dnorm(0, 0.4)T(-10,10) #prior for coefficient on recapture in the the same occasion (trap happy/trap shy) equivalent to c
+    sigma.0 ~ dnorm(0, 0.4)T(-10,10)   #prior for intercept on p
+    sigma.recap ~ dnorm(0, 0.4)T(-10,10) #prior for coefficient on recapture in the the same occasion equivalent to c
     sigma.male ~ dnorm(0, 0.4)T(-10,10)   #prior for coef on sex=male
-    for(m in 1:(length(uniq.months)-1)){
-      sigma.month[m] ~ dnorm(0, 0.4)T(-10,10)   #priors for months
-    }
     
+    for(mo in 1:11){
+      sigma.month[mo] ~ dnorm(0, 0.4)T(-10,10)   #prior for months
+    }
+    sigma.month.use <- c(0,sigma.month)
+
+
     # Model for Phi
     for(i in 1:nind){
       for(w in 1:(n.weeks-1)){  
         # phi has  2 dimensions [indiv, and weeks]
-        cmonth <- covariate.month[w] # current month
+        
 
         logit(phi[i,w]) <- 
           alpha.0 + 
-          alpha.ndvi_0 * ndvi_0[i,w] + 
           alpha.ndvi_1 * ndvi_1[i,w] + 
           alpha.male * sex[i] +         #0 if female, 1 if male
-          ifelse(cmonth==uniq.months[1],0, alpha.month[which(uniq.months==cmonth)-1]) +
-          ifelse(cmonth==uniq.months[1],0, alpha.month.ndvi_1[which(uniq.months==cmonth)-1]) * ndvi_1[i,w]
-
+          alpha.month.use[covariate.month[w]] +
+          alpha.month.ndvi_1.use[covariate.month[w]] * ndvi_1[i,w]
+      
       } #w for weeks
     } #i for individual
     
-    # Model for p
+    # Model for p - 3 dimensions [indiv, week, day] 
     for(i in 1:nind){
-      for(w in 1:n.weeks){ 
-        for(d in 1:n.sec.occ[w]){
-        # p has 3 dimensions [indiv, week, day] 	
+      for(w in weeks.trapped){
+        for(d in 1:n.sec.occ[covariate.prim[w]]){
           logit(p[i,w,d]) <- 
             sigma.0 +             # intercept
             sigma.recap * p.or.c[i,w,d] + #adjustment for if animal was caught previously in this primary session (0 if not caught before and 1 if so)
-            sigma.male + sex[i] +   # adjustment for males (0 if female)
-            ifelse(cmonth==uniq.months[1],0, sigma.month[which(uniq.months==cmonth)-1]) # month factor, where first unique month=0
+            sigma.male * sex[i] +   # adjustment for males (0 if female)
+            sigma.month.use[covariate.month[w]] # month factor, where Jan=0
         } #d for days
       } #w for weeks
     } #i for individual
